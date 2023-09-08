a062c38f68c48a6ad7990bfeb69dbf3ebda0a894
Nasma2022 <nasmaaboubakary@gmail.com>
2023-09-08 13:40:38 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+/**
+ * main - Prints the alphabet at reverse
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
