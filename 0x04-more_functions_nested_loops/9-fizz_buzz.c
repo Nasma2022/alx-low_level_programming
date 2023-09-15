66f2d25a3b59d58f3c3ad67f736c74d15ff2088e
Nasma2022 <nasmaaboubakary@gmail.com>
2023-09-14 19:44:18 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,5 +23,16 @@ int main(void)
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
+			}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
-		else if (i ==
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
